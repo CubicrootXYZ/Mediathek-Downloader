34f1aaf479390bfc7080ff72141e91c4bd98b334
Cubicroot <github@cubicroot.xyz>
2021-02-12 17:22:51 +0100

    added debugging

--- run.py
+++ run.py
@@ -181,12 +181,12 @@ class Downloader:
 
             if "min_duration" in job.keys():
                 if int(job["min_duration"]) < int(infos["duration"]):
-                    print(f"{self.prä} {infos['title']} does not match min duration")
+                    print(f"{self.prä} {infos['title']} {infos['duration']} does not match min duration")
                     return False
 
             if "max_duration" in job.keys():
                 if int(job["max_duration"]) > int(infos["duration"]):
-                    print(f"{self.prä} {infos['title']} does not match max duration")
+                    print(f"{self.prä} {infos['title']} {infos['duration']} does not match max duration")
                     return False
 
             path = self.config["mediathek_path"] + "/" + self._cleanFileName(str(job["category"])) + "/" + self._cleanFileName(str(job["shortname"]))
